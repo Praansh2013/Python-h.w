49dbbbce536f1e605d621699fb61b5b86a2b60dc
Praansh2013 <kapoorpraansh@gmail.com>
2025-06-01 19:15:31 +0530

    initial commit

---
+++ natural.py
@@ -0,0 +1,6 @@
+print("Enter a number below, I will tell is it a natural number or not")
+n=int(input("Enter number here "))
+if n>0:
+    print(n," Is a natural number")
+else:
+    print(n," Is not a natural number")
