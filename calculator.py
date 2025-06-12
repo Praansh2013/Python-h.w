fab3162cde5fef6f0c8266c162bcf22a856f2dd6
Praansh2013 <kapoorpraansh@gmail.com>
2025-06-12 17:00:41 +0530

    initial commit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+print("Enter number a and b, I will add them")
+def add(a,b):
+    print("The sum of a and b is: ",a+b)
+
+a=int(input("Enter number a"))
+b=int(input("Enter number b"))
+
+print("\n Enter number m and n, I will multiply them")
+def multi(m,n):
+    print("The product of m and n is: ",m*n)
+    
+m=int(input("Enter number m"))
+n=int(input("Enter number n"))
